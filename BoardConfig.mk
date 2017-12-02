e7ae3160c7bad885429fc0f181e2a365a36d5365
Anik1199 <anik@lineageos.org>
2017-12-02 12:58:45 +0600

    kiwi: Enable SVELTE memory configuration
    Change-Id: Ief73ebb39268133494079fe773822b503b409fa1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,9 @@ TARGET_PROVIDES_LIBLIGHT := true
 # Lineage Hardware
 BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
 
+# Malloc
+MALLOC_SVELTE := true
+
 # Partitions
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
