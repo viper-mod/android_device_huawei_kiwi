4c7bb415e02f447050b867dfadc444787452d0a2
jsbeyond <67664518@qq.com>
2019-01-28 15:11:31 +0800

    Viper-ify

--- viper_kiwi.mk
+++ viper_kiwi.mk
@@ -14,10 +14,13 @@
 
 $(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Viper stuff.
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_kiwi
+# Its Official ViperOS
+ VIPER_BUILD_TYPE := OFFICIAL
+
+PRODUCT_NAME := viper_kiwi
 BOARD_VENDOR := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
